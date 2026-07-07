8cd8267bb372f58738c09a28ccc19e7cba58440e
25mx218 <25mx218@MCA.PSG>
2026-07-07 11:28:30 +0530

    Added search and delete student features

--- src/com/student/StudentManager.java
+++ src/com/student/StudentManager.java
@@ -6,10 +6,12 @@ public class StudentManager {
 
     ArrayList<Student> students = new ArrayList<>();
 
+    // Add Student
     public void addStudent(Student student) {
         students.add(student);
     }
 
+    // Display Students
     public void displayStudents() {
 
         if (students.isEmpty()) {
@@ -20,5 +22,35 @@ public class StudentManager {
         for (Student s : students) {
             System.out.println(s);
         }
+    }
+
+    // Search Student
+    public void searchStudent(int id) {
+
+        for (Student s : students) {
+
+            if (s.id == id) {
+                System.out.println("Student Found");
+                System.out.println(s);
+                return;
+            }
+        }
+
+        System.out.println("Student Not Found");
+    }
+
+    // Delete Student
+    public void deleteStudent(int id) {
+
+        for (Student s : students) {
+
+            if (s.id == id) {
+                students.remove(s);
+                System.out.println("Student Deleted Successfully");
+                return;
+            }
+        }
+
+        System.out.println("Student Not Found");
     }
 }
